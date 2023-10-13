6a948580030bdc1885eafc6fa58a64ce86c573a1
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-10-13 23:21:25 +0200

    now need to fix the filenames

--- content.js
+++ content.js
@@ -1,41 +1,87 @@
-let highlighting = false;
+let isToggled = false;
+let highlightedElement;
 
-chrome.runtime.onMessage.addListener(function(message) {
-    console.log("Message received in content.js:", message);
+function init() {
+    // Listen to messages from the popup to toggle the state
+    chrome.runtime.onMessage.addListener((message) => {
+        if (message === 'toggle') {
+            isToggled = !isToggled;
+            console.log("Toggled State via popup:", isToggled);
+            if (!isToggled && highlightedElement) {
+                removeHighlight(highlightedElement);
+            }
+        }
+    });
+}
 
-    if (message === 'toggle') {
-        chrome.storage.local.get(['isToggled'], function(result) {
-            highlighting = result.isToggled;
-            console.log("Highlighting state set to:", highlighting);
-        });
+document.addEventListener('keydown', function(event) {
+    if (event.key === 't') {
+        const activeElement = document.activeElement;
+        if (activeElement && activeElement.tagName && 
+            (activeElement.tagName.toLowerCase() !== 'input' && 
+             activeElement.tagName.toLowerCase() !== 'textarea')) {
+            isToggled = !isToggled;
+            console.log("Toggled State via keypress:", isToggled);
+        }
     }
 });
 
-document.addEventListener('mouseover', function(e) {
-    if (highlighting) {
-        e.target.style.border = "2px solid red";
-        console.log("Mouse over:", e.target);
+document.addEventListener('mouseover', (event) => {
+    if (isToggled && event.target && event.target.tagName && 
+        event.target.tagName.toLowerCase() === 'div') {
+        console.log("Mouse entered a div. Adding highlight to:", event.target);
+        addHighlight(event.target);
     }
 });
 
-document.addEventListener('mouseout', function(e) {
-    if (highlighting) {
-        e.target.style.border = "";
-        console.log("Mouse out:", e.target);
+document.addEventListener('mouseout', (event) => {
+    if (event.target && event.target.tagName && 
+        event.target.tagName.toLowerCase() === 'div') {
+        console.log("Mouse left a div. Removing highlight from:", event.target);
+        removeHighlight(event.target);
     }
 });
 
-document.addEventListener('click', function(e) {
-    if (highlighting) {
-        console.log("Click detected on:", e.target);
-        let text = e.target.innerText || e.target.textContent;
-        let filename = `${Date.now()}_${document.title.replace(/[\/:*?"<>|]/g, '_')}.txt`;
-        let blob = new Blob([text], {type: 'text/plain'});
-
-        let url = URL.createObjectURL(blob);
-        chrome.runtime.sendMessage({content: url, filename: filename});
-
-        e.preventDefault();
-        e.stopPropagation();
+document.addEventListener('mousedown', (event) => {
+    if (event.button !== 0) return;
+    console.log("Clicked on an element:", event.target);
+    if (event.target.tagName && event.target.tagName.toLowerCase() === 'div') {
+        console.log("Clicked on a div.");
+        if (isToggled) {
+            console.log("Clicked on a highlighted div.");
+            const divText = event.target.innerText || event.target.textContent;
+            downloadText(divText);
+        }
     }
 });
+
+function addHighlight(element) {
+    if (highlightedElement) {
+        removeHighlight(highlightedElement);
+    }
+    highlightedElement = element;
+    element.style.outline = '2px solid red';
+    console.log("Highlighted element:", element);
+}
+
+function removeHighlight(element) {
+    if (element) {
+        element.style.outline = '';
+        console.log("Removed highlight from element:", element);
+    }
+}
+
+function downloadText(text) {
+    const blob = new Blob([text], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `text-${Date.now()}.txt`;
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+    URL.revokeObjectURL(url);
+}
+
+// Initialize the state and listeners
+init();
--- popup.js
+++ popup.js
@@ -1,14 +1,60 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("DOMContentLoaded for popup.js");
+    const UIController = {
+        elements: {
+            toggleButton: document.getElementById('toggle'),
+            locationInput: document.getElementById('location'),
+            saveButton: document.getElementById('save')
+        },
 
-    document.getElementById('toggle').addEventListener('click', function() {
-        console.log("Toggle button clicked in popup.js");
+        updateToggleButtonText(isToggled) {
+            this.elements.toggleButton.innerText = `Toggle Highlight (${isToggled ? 'ON' : 'OFF'})`;
+        },
 
-        chrome.storage.local.get(['isToggled'], function(result) {
-            const newState = !result.isToggled;
-            console.log("Setting toggled state to:", newState);
-            chrome.storage.local.set({isToggled: newState});
-            chrome.runtime.sendMessage('toggle');
-        });
-    });
+        updateLocationInput(value) {
+            this.elements.locationInput.value = value || '';
+        },
+
+        initialize() {
+            // Get initial states from storage
+            chrome.storage.sync.get(['defaultLocation', 'isToggled'], data => {
+                this.updateLocationInput(data.defaultLocation);
+                const isToggled = data.isToggled || false;
+                this.updateToggleButtonText(isToggled);
+            });
+        }
+    };
+
+    const EventHandlers = {
+        handleToggleClick() {
+            chrome.storage.local.get('isToggled', data => {
+                const newState = !data.isToggled;
+                chrome.storage.local.set({ isToggled: newState }, () => {
+                    UIController.updateToggleButtonText(newState);
+                    chrome.runtime.sendMessage('toggle');
+                    
+                    // Execute the toggleHighlight function in the content script
+                    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+                        chrome.scripting.executeScript({
+                            target: { tabId: tabs[0].id },
+                            function: toggleHighlight,  // assuming toggleHighlight function is present in content script
+                        });
+                    });
+                });
+            });
+        },
+
+        handleSaveClick() {
+            const location = UIController.elements.locationInput.value;
+            chrome.storage.sync.set({ defaultLocation: location }, () => {
+                console.log('Default download location saved:', location);
+            });
+        }
+    };
+
+    // Event Listener Bindings
+    UIController.elements.toggleButton.addEventListener('click', EventHandlers.handleToggleClick);
+    UIController.elements.saveButton.addEventListener('click', EventHandlers.handleSaveClick);
+
+    // Initialize UI state
+    UIController.initialize();
 });
