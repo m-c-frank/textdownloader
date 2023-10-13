eaf12987b9f40bcfe6d76c0a74f816ab361a62c0
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-10-13 21:11:20 +0200

    added suggestions from chatgpt

--- content.js
+++ content.js
@@ -1,1 +1,15 @@
-// This can remain empty for now, but can be expanded upon if more functionality is required in the future.
+let hoveredElement = null;
+
+document.addEventListener('mouseover', function(event) {
+    if (hoveredElement) {
+        hoveredElement.style.outline = '';
+    }
+    hoveredElement = event.target;
+    hoveredElement.style.outline = '2px solid red';
+});
+
+document.addEventListener('mouseout', function(event) {
+    if (hoveredElement) {
+        hoveredElement.style.outline = '';
+    }
+});
--- popup.js
+++ popup.js
@@ -1,12 +1,19 @@
 document.getElementById('downloadText').addEventListener('click', function() {
-  chrome.tabs.executeScript({
-    code: 'window.getSelection().toString()'
-  }, function(selection) {
-    const blob = new Blob([selection[0]], {type: 'text/plain'});
-    const url = URL.createObjectURL(blob);
-    chrome.downloads.download({
-      url: url,
-      filename: 'selectedText.txt'
+    chrome.tabs.executeScript({
+        code: `
+            let text = '';
+            const el = document.querySelector('div[style*="outline: 2px solid red"]');
+            if(el) {
+                text = el.innerText || el.textContent;
+            }
+            text;
+        `
+    }, function(selection) {
+        const blob = new Blob([selection[0]], {type: 'text/plain'});
+        const url = URL.createObjectURL(blob);
+        chrome.downloads.download({
+            url: url,
+            filename: 'selectedText.txt'
+        });
     });
-  });
 });
