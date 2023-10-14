873503981e92ed1512ac8dd98aad1f691eb6f93b
Martin Christoph Frank <martin7.frank7@gmail.com>
2023-10-14 13:13:10 +0200

    shift+t mode working now as intended

--- eventHandlers.js
+++ eventHandlers.js
@@ -1,9 +1,17 @@
+const State = {
+    INITIAL: 'initial',
+    SELECTION: 'selection',
+    NORMAL: 'normal',
+};
+
+let currentState = State.INITIAL;
+
 function handleKeyDown(event) {
-    console.debug("handleKeyDown triggered", event);
+    log("handleKeyDown triggered", event);
     if (event.keyCode === 27) {  // ESC key
         const highlightedElement = getHighlightedElement();
         if (highlightedElement) {
-            console.debug("Removing highlight in handleKeyDown");
+            log("Removing highlight in handleKeyDown");
             removeHighlight(highlightedElement);
         }
         const selectedDivs = document.querySelectorAll('.selected-div');
@@ -11,48 +19,62 @@ function handleKeyDown(event) {
         hideSelectionOptions();
         isHighlightMode = false;
         document.body.style.userSelect = "";
+        currentState = State.NORMAL;  // Return to NORMAL state after ESC key
     }
 
-    if (event.shiftKey && event.key.toLowerCase() === 't') {
+    if (event.shiftKey && event.key.toLowerCase() === 't' && currentState !== State.SELECTION) {
         isHighlightMode = true;
         document.body.style.userSelect = "none";
     }
 }
 
 function handleKeyUp(event) {
-    console.debug("handleKeyUp triggered", event);
-    if (event.shiftKey && event.key.toLowerCase() === 't') {
+    log("handleKeyUp triggered", event);
+    if (!event.shiftKey || event.key.toLowerCase() === 't') {
         isHighlightMode = false;
         document.body.style.userSelect = "";
+        deselectAll();
     }
 }
 
+function deselectAll() {
+    const highlightedElement = getHighlightedElement();
+    if (highlightedElement) {
+        removeHighlight(highlightedElement);
+    }
+    const selectedDivs = document.querySelectorAll('.selected-div');
+    selectedDivs.forEach(div => div.classList.remove('selected-div'));
+    hideSelectionOptions();
+    currentState = State.NORMAL;
+}
+
 function handleMouseOver(event) {
-    console.debug("handleMouseOver triggered", event.target);
-    if (isHighlightMode && event.target && event.target.tagName &&
+    log("handleMouseOver triggered", event.target);
+    if (isHighlightMode && currentState !== State.SELECTION && event.target && event.target.tagName &&
         event.target.tagName.toLowerCase() === 'div') {
         addHighlight(event.target);
     }
 }
 
 function handleMouseOut(event) {
-    console.debug("handleMouseOut triggered", event.target);
-    if (isHighlightMode && event.target && event.target.tagName &&
+    log("handleMouseOut triggered", event.target);
+    if (isHighlightMode && currentState !== State.SELECTION && event.target && event.target.tagName &&
         event.target.tagName.toLowerCase() === 'div') {
         removeHighlight(event.target);
     }
 }
 
 function handleMouseDown(event) {
-    console.debug("handleMouseDown triggered", event);
+    log("handleMouseDown triggered", event);
     const highlightedElement = getHighlightedElement();
-    if (isHighlightMode && highlightedElement) {
+    if (isHighlightMode && currentState !== State.SELECTION && highlightedElement) {
         selectElement(highlightedElement);
+        currentState = State.SELECTION;  // Move to SELECTION state
     }
 }
 
 function addHighlight(element) {
-    console.debug("Adding highlight to element", element);
+    log("Adding highlight to element", element);
     const uniqueId = Date.now().toString();
     element.setAttribute(HIGHLIGHT_DATA_ATTRIBUTE, uniqueId);
     currentHighlightId = uniqueId;
@@ -60,7 +82,7 @@ function addHighlight(element) {
 }
 
 function removeHighlight(element) {
-    console.debug("Removing highlight from element", element);
+    log("Removing highlight from element", element);
     if (element.getAttribute(HIGHLIGHT_DATA_ATTRIBUTE) === currentHighlightId) {
         element.classList.remove('highlighted-div');
         currentHighlightId = null;
@@ -69,20 +91,28 @@ function removeHighlight(element) {
 
 function getHighlightedElement() {
     const element = document.querySelector(`[${HIGHLIGHT_DATA_ATTRIBUTE}="${currentHighlightId}"]`);
-    console.debug("Fetching highlighted element", element);
+    log("Fetching highlighted element", element);
     return element;
 }
 
 function selectElement(element) {
-    console.debug("Selecting element", element);
+    log("Selecting element", element);
     removeHighlight(element);
     element.classList.add('selected-div');
     displaySelectionOptions();
 }
 
-function displaySelectionOptions() {
-    console.debug("Displaying selection options");
-    const optionsDiv = getOptionsDiv();
-    optionsDiv.style.display = 'block';
-    document.body.appendChild(optionsDiv);
+function deselectHandler() {
+    const highlightedElement = getHighlightedElement();
+    if (highlightedElement) {
+        highlightedElement.classList.remove('selected-div');
+        hideSelectionOptions();
+        currentState = State.NORMAL; // Return to NORMAL state
+    }
+}
+
+function log(...args) {
+    if (isHighlightMode) {
+        console.debug(...args);
+    }
 }
